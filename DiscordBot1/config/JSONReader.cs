a3914cffa0ebeb8ce1e8a48bd1dd9558d4739c9a
jpsha <jpsha@POW-PUTER>
2024-11-24 17:47:22 +0200

    Add project files.

---
+++ DiscordBot1/config/JSONReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DiscordBot1.config
+{
+    internal class JSONReader
+    {
+        public string token {  get; set; }
+        public string prefix { get; set; }
+
+        public async Task ReadJSON()
+        {
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+                JSONStruc data = JsonConvert.DeserializeObject<JSONStruc>(json);
+
+                this.token = data.token; 
+                this.prefix = data.prefix;
+            }
+        }
+    }
+
+    internal sealed class JSONStruc
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+}
